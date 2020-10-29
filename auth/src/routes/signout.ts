e707f92fcef5a3e0714fd6d713008db7dee89439
Maurizio Sodano <maurizio.sodano@soulsoftware.it>
2020-10-29 10:21:32 +0100

    Added express validator

---
+++ auth/src/routes/signout.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const router=express.Router();
+
+router.post("/api/users/signout", (req,res)=>{
+    res.send("Hi there! from current user");
+});
+
+
+export {router as signoutRouter};
