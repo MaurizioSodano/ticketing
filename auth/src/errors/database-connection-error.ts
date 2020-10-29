e00fc22c928e6f1bb41f0c2b1543ff6b5cf9e7c9
Maurizio Sodano <maurizio.sodano@soulsoftware.it>
2020-10-29 16:56:11 +0100

    added abstract CustomError class

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,14 +1,17 @@
 
-export class DatabaseConnectionError extends Error {
-    reason="Error connecting to database";
-    
-    constructor(){
-        super();
+import {CustomError} from "./custom-error";
+
+export class DatabaseConnectionError extends CustomError {
+    reason = "Error connecting to database";
+    statusCode = 500;
+    constructor() {
+        super("Error connecting to database");
 
         // Only because we are extending a built in class
-        Object.setPrototypeOf(this,DatabaseConnectionError.prototype);
+        Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+    }
 
-
-
+    serializeErrors() {
+        return [{ message: this.reason }]
     }
 }
--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,12 +1,21 @@
-import {ValidationError} from "express-validator";
+import { ValidationError } from "express-validator";
+import {CustomError} from "./custom-error";
 
-export class RequestValidationError extends Error {
-    constructor(public errors:ValidationError[]){
-        super();
+export class RequestValidationError extends CustomError {
+    statusCode = 400;
+
+    constructor(public errors: ValidationError[]) {
+        super("invalid request parameters");
 
         // Only because we are extending a built in class
-        Object.setPrototypeOf(this,RequestValidationError.prototype);
+        Object.setPrototypeOf(this, RequestValidationError.prototype);
+    }
 
+    serializeErrors() {
+      
+        return this.errors.map(error => {
+            return { message: error.msg, field: error.param };
+        });
 
 
     }
