5d04e61617bbdde4dd2361e3409e4b143d41ebfe
Maurizio Sodano <maurizio.sodano@soulsoftware.it>
2020-10-30 08:34:54 +0100

    connecting to mongoDb

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,15 +1,18 @@
 import express from "express";
 import "express-async-errors";
 
-import {json} from "body-parser";
-import {currentUserRouter} from "./routes/current-user";
-import {signinRouter} from "./routes/signin";
-import {signoutRouter} from "./routes/signout";
-import {signupRouter} from "./routes/signup";
-import {errorHandler} from "./middlewares/error-handler";
+import { json } from "body-parser";
+
+import mongoose from "mongoose";
+
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from './errors/not-found-error';
 
-const app=express();
+const app = express();
 app.use(json());
 
 app.use(currentUserRouter);
@@ -17,11 +20,27 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.all('*', async (req,res,next)=>{
-    next( new NotFoundError());
+app.all('*', async (req, res, next) => {
+    next(new NotFoundError());
 })
 app.use(errorHandler);
 
-app.listen(3000,()=>{
+const start = async () => {
+    try {
+        await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.log("Connected to MongoDb");
+    } catch (error) {
+        console.log(error);
+    }
+
+};
+
+start();
+
+app.listen(3000, () => {
     console.log("Auth Service listening on port 3000!!!");
 })
