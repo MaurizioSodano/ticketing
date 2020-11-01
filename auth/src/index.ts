00cf3bdd261ed34212b5a085fa84027eaff8d59a
Maurizio Sodano <maurizio.sodano@soulsoftware.it>
2020-11-01 11:49:36 +0100

    Added JWT and cookie in signup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,8 @@ import "express-async-errors";
 import { json } from "body-parser";
 
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
+
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -13,8 +15,12 @@ import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.set("trust proxy", true);
 app.use(json());
-
+app.use(cookieSession({
+    signed: false,
+    secure: true
+}))
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
@@ -26,6 +32,10 @@ app.all('*', async (req, res, next) => {
 app.use(errorHandler);
 
 const start = async () => {
+
+    if (!process.env.JWT_KEY) {
+        throw new Error("JWT_KEY must be defined");
+    }
     try {
         await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
             useNewUrlParser: true,
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
 
-import {BadRequestError} from "../errors/bad-request-error";
-import { RequestValidationError} from "../errors/request-validation-error";
-import {User} from "../models/user"
+
+import { BadRequestError } from "../errors/bad-request-error";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { User } from "../models/user"
 
 const router = express.Router();
 
@@ -22,16 +24,28 @@ router.post("/api/users/signup", [
         throw new RequestValidationError(errors.array());
     }
 
-    const { email,password } = req.body;
+    const { email, password } = req.body;
 
-    const existingUser =await User.findOne({email});
+    const existingUser = await User.findOne({ email });
 
-    if (existingUser){
-       throw new BadRequestError('Email in use');
+    if (existingUser) {
+        throw new BadRequestError('Email in use');
     }
 
-    const user= User.build({email,password});
+    const user = User.build({ email, password });
     await user.save();
+
+    //generate JWT
+
+    const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email
+    }, process.env.JWT_KEY!);
+
+    //Store it in session object
+
+    req.session = { jwt: userJwt };
+
     return res.status(201).send(user);
 
 });
