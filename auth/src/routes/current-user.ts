d62a15e20acc669995e5257d9407f124959e8e34
Maurizio Sodano <maurizio.sodano@soulsoftware.it>
2020-11-01 15:27:45 +0100

    adding current user auth

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,10 +1,22 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
-const router=express.Router();
+const router = express.Router();
 
-router.get("/api/users/currentuser", (req,res)=>{
-    res.send("Hi there! from current user");
+router.get("/api/users/currentuser", (req, res) => {
+    if (!req.session?.jwt) {
+        return res.send({ currentUser: null });
+    }
+
+    try {
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+        return res.send({ currentUser: payload });
+    } catch (error) {
+        return res.send({ currentUser: null });
+    }
+
+
 });
 
 
-export {router as currentUserRouter};
+export { router as currentUserRouter };
