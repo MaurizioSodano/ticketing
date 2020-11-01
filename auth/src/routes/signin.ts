66e771ae144602018c7281d5938347485b333de0
Maurizio Sodano <maurizio.sodano@soulsoftware.it>
2020-11-01 12:19:44 +0100

    common middw

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
+import { body } from "express-validator";
 
-import { RequestValidationError } from "../errors/request-validation-error";
 
+import { validateRequest } from "../middlewares/validate-request";
 import jwt from "jsonwebtoken";
 
 const router = express.Router();
@@ -15,16 +15,13 @@ router.post("/api/users/signin", [
         .trim()
         .notEmpty()
         .withMessage("You must supply a password")
-], (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
-    }
+],
+    validateRequest,
+    (req: Request, res: Response) => {
 
 
-    res.send("sign in current user");
-});
+        res.send("sign in current user");
+    });
 
 
 export { router as signinRouter };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,10 +1,10 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
+import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 
-
+import { validateRequest } from "../middlewares/validate-request";
 import { BadRequestError } from "../errors/bad-request-error";
-import { RequestValidationError } from "../errors/request-validation-error";
+
 import { User } from "../models/user"
 
 const router = express.Router();
@@ -17,38 +17,35 @@ router.post("/api/users/signup", [
         .trim()
         .isLength({ min: 4, max: 20 })
         .withMessage("Password must be between 4 and 20 characters")
-], async (req: Request, res: Response) => {
-    const errors = validationResult(req);
+],
+    validateRequest,
+    async (req: Request, res: Response) => {
 
-    if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
-    }
+        const { email, password } = req.body;
 
-    const { email, password } = req.body;
+        const existingUser = await User.findOne({ email });
 
-    const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            throw new BadRequestError('Email in use');
+        }
 
-    if (existingUser) {
-        throw new BadRequestError('Email in use');
-    }
+        const user = User.build({ email, password });
+        await user.save();
 
-    const user = User.build({ email, password });
-    await user.save();
+        //generate JWT
 
-    //generate JWT
+        const userJwt = jwt.sign({
+            id: user.id,
+            email: user.email
+        }, process.env.JWT_KEY!);
 
-    const userJwt = jwt.sign({
-        id: user.id,
-        email: user.email
-    }, process.env.JWT_KEY!);
+        //Store it in session object
 
-    //Store it in session object
+        req.session = { jwt: userJwt };
 
-    req.session = { jwt: userJwt };
+        return res.status(201).send(user);
 
-    return res.status(201).send(user);
-
-});
+    });
 
 
 export { router as signupRouter };
