2649dff49ca78cf7cae2b94fd333f5628cc3e259
Maurizio Sodano <maurizio.sodano@soulsoftware.it>
2020-11-04 10:11:47 +0100

    added OnSuccess callback

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import axios from "axios";
 
-export default ({ url, method, body }) => {
+export default ({ url, method, body, onSuccess }) => {
   const [errors, setErrors] = useState([]);
 
   const doRequest = async () => {
     try {
       setErrors([]);
       const response = await axios[method](url, body);
-
+      if (onSuccess) {
+        onSuccess(response.data);
+      }
       return response.data;
     } catch (err) {
       setErrors(
