ffc9f631dc8bfa479185fa55e8b3c5211cfb3bc7
Maurizio Sodano <maurizio.sodano@soulsoftware.it>
2020-11-07 19:16:41 +0100

    relocating common code and importing it

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -3,7 +3,7 @@ import "express-async-errors";
 
 import { json } from "body-parser";
 
-import mongoose from "mongoose";
+
 import cookieSession from "cookie-session";
 
 
@@ -11,15 +11,15 @@ import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from './errors/not-found-error';
+import { errorHandler, NotFoundError } from "@msticketingudemy/common";
+
 
 const app = express();
 app.set("trust proxy", true);
 app.use(json());
 app.use(cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV!=="test"
+    secure: process.env.NODE_ENV !== "test"
 }))
 app.use(currentUserRouter);
 app.use(signinRouter);
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,8 +2,8 @@ import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 
-import { validateRequest } from "../middlewares/validate-request";
-import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest, BadRequestError } from "@msticketingudemy/common";
+
 
 import { User } from "../models/user"
 
