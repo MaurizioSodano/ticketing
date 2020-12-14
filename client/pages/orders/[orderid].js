bd75b2cf6dbdab88a0f823cc7c38d74a0715c3ab
Maurizio Sodano <maurizio.sodano@soulsoftware.it>
2020-12-14 14:16:06 +0100

    added timer for expiration

--- client/pages/orders/[orderid].js
+++ client/pages/orders/[orderid].js
@@ -1,11 +1,30 @@
-const OrderShow = ({ orderid }) => {
-  return <div>OrderShow: {orderid}</div>;
+import { useEffect, useState } from 'react';
+
+const OrderShow = ({ order }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
+  if (timeLeft < 0) {
+    return <div>Order Expired</div>;
+  }
+  return <div> {timeLeft} seconds until order expires</div>;
 };
 
 OrderShow.getInitialProps = async (context, client) => {
   const { orderid } = context.query; //comes from [orderid]
-
-  return { orderid };
+  const { data } = await client.get(`/api/orders/${orderid}`);
+  return { order: data };
 };
 
 export default OrderShow;
